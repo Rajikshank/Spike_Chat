c16439de766a68e011210889cab7c2997c3d7317
Rajikshank <80474719+Rajikshank@users.noreply.github.com>
2023-02-17 23:24:04 +0530

    friday

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import http  from 'http'
+import {Server} from 'socket.io'
 
 const app=express();
-const port=4000;
+const port=4000; 
+const httpServer=http.createServer(app)
+const io=new Server(httpServer)
+
+
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 
 
 app.get('/',(req,res)=>{
-    res.json({data:"hello world from socket"})
+    res.sendFile(__dirname + '/index.html');
 })
 
 
-app.listen(port,()=>{
-    console.log('Server is running');
+
+io.on('connection',(socket)=>{
+    console.log("connection is Ready")
+})
+
+httpServer.listen(port,()=>{
+    console.log('Server is running ');
 })
