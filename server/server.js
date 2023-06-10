7e629f293b7ee5901cf99e35b79f16589cde493b
Rajikshank <80474719+Rajikshank@users.noreply.github.com>
2023-06-11 00:09:07 +0530

    hey

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import http  from 'http'
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 
 const app=express();
 const port=4000; 
@@ -20,14 +20,29 @@ const __dirname=path.dirname(__filename)
 
 
 app.get('/',(req,res)=>{
+    console.log(__filename)
     res.sendFile(__dirname + '/index.html');
 })
 
 
 
 io.on('connection',(socket)=>{
+
     console.log("connection is Ready")
+    socket.on('send-message',(msg)=>{
+        console.log("message received"+msg )
+        socket.broadcast.emit('message-from-server',{msg})
+    })
 })
+
+io.on('disconnect',(sockboet)=>{
+
+    console.log("user left")
+     
+})
+
+
+
 
 httpServer.listen(port,()=>{
     console.log('Server is running ');
